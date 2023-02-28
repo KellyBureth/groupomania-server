d9083d05ee27dcaae23cbf6c91e3bbed07e40057
Kelly Bureth <96128478+KellyBureth@users.noreply.github.com>
2023-02-28 20:07:41 +0100

    secure true

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -28,7 +28,7 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge, sameSite: 'none' });
+    res.cookie("jwt", token, { httpOnly: true, maxAge, secure : true });
     res.status(200).json({ user: user._id });
   } catch (err) {
     const errors = signInErrors(err);
