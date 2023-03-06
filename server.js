51f2a9412bf8022e690231ace9dd2c061afb7e9c
Kelly Bureth <96128478+KellyBureth@users.noreply.github.com>
2023-03-06 20:58:20 +0100

    front url on ressetheader

--- server.js
+++ server.js
@@ -34,20 +34,20 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-//no header brody test
-//app.use((req, res, next) => {
-//  res.setHeader("Access-Control-Allow-Origin", "*"); // d'accéder à notre API depuis n'importe quelle origine ( '*' ) donc tout le monde peut acceder à l'api
-//  res.setHeader(
-//    "Access-Control-Allow-Headers",
-//    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//  ); //autorisation d'ajouter les headers mentionnés aux requêtes envoyées vers notre API  (Origin , X-Requested-With , etc.) ;
-//  res.setHeader("Access-Control-Allow-Credentials", "true");
-//  res.setHeader(
-//    "Access-Control-Allow-Methods",
-//    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//  ); //autorisation d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
-//  next();
-//});
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://groupomania-intranet.netlify.app"); // d'accéder à notre API depuis n'importe quelle origine ( '*' ) donc tout le monde peut acceder à l'api
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  ); //autorisation d'ajouter les headers mentionnés aux requêtes envoyées vers notre API  (Origin , X-Requested-With , etc.) ;
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  ); //autorisation d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+  next();
+});
 
 // server
 app.listen(process.env.PORT || 3000, () => {
