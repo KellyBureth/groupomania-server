74e49a7dafa83e4004e6a011c34d4ec255ed3a54
Kelly Bureth <96128478+KellyBureth@users.noreply.github.com>
2023-03-07 22:29:52 +0100

    no redirect

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -41,6 +41,6 @@ module.exports.signIn = async (req, res) => {
 module.exports.logout = (req, res) => {
   //res.cookie("jwt", "", { maxAge: 1 }); //dure 1ms
   res.clearCookie('jwt');
-  res.redirect("/");
+  //res.redirect("/");
   console.log("resclearCookie");
 };
