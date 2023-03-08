480d910f85bf4331a0d07596f760253c8a448acd
Kelly Bureth <96128478+KellyBureth@users.noreply.github.com>
2023-03-08 14:22:25 +0100

    Update upload.controller.js

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -32,8 +32,8 @@ module.exports.uploadProfil = async (req, res) => {
   try {
     await UserModel.findByIdAndUpdate(
       req.body.userId,
-      { $set: { picture:  "../client/public/uploads/profil/" + fileName } },
-     // { $set: { picture: "./uploads/profil/" + fileName } },
+      //{ $set: { picture:  "../client/public/uploads/profil/" + fileName } }, //same error
+    { $set: { picture: "./uploads/profil/" + fileName } },
       { new: true, upsert: true, setDefaultsOnInsert: true }
     )
       .then((data) => res.send(data))
